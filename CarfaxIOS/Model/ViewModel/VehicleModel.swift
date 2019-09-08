9de6a02da87b6e6518e8cfc7c52d9d81cce79ce7
Vipul Arvind <varvind@dminc.com>
2019-09-07 23:51:38 -0400

    Moved DataModel to appropriate File

--- CarfaxIOS/Model/ViewModel/VehicleModel.swift
+++ CarfaxIOS/Model/ViewModel/VehicleModel.swift
@@ -13,20 +13,63 @@ import Foundation
 //      Model for 1 Vehicle
 //
 
-struct VehicleModel: Codable {
-    
-  // MARK: - Vars
-  
-  let model: String
-  let make: String
-  let year: Int
-  let onePrice: Int
-  let mileage: Int
-  let city: String
-  let state: String
-  let phone: String
+struct VehicleImages: Codable {
+  var large: String
+  var medium: String
+  var small: String
 }
 
-struct VehiclesList: Codable {
-  let vehicles: [VehicleModel]
+struct Dealer: Codable {
+  var city: String
+  var state: String
+  var zip: String
+  var phone: String
+}
+
+struct Response: Codable {
+  
+  struct Vehicle: Codable {
+    var year: Int
+    var make: String
+    var model: String
+    var trim: String
+    var listPrice: Int
+    var mileage: Int
+    
+    var dealer: Dealer
+    
+    func formattedYearMakeModelTrim () -> String {
+      return String(year) + " " + make + model + trim
+    }
+    
+    func formattedPriceMileageLocation () -> NSAttributedString {
+      
+      let StringPrice = listPrice.currencyFormat
+      let StringMileage = mileage.abbreviated      
+      let mileageCityState = " | " + StringMileage + "Mi | " + dealer.city + ", " + dealer.state
+      
+      //now we want the price to be bold and rest of the string as regular font
+      let attributesBold: [NSAttributedString.Key: Any] = [
+        .font: Constants.systemFontBoldSize12,
+        .foregroundColor: Constants.blackColor
+      ]
+      
+      let attributesRegular: [NSAttributedString.Key: Any] = [
+        .font: Constants.systemFontSize12,
+        .foregroundColor: Constants.blackColor
+      ]
+      
+      let attributedString = NSMutableAttributedString(string: StringPrice, attributes: attributesBold)
+      let attributedStringForMileageCityState = NSAttributedString(string: mileageCityState, attributes: attributesRegular)
+      
+      attributedString.append(attributedStringForMileageCityState)
+      return attributedString
+    }
+    
+    func formattedPhoneNumber () -> String? {
+      return dealer.phone.formattedPhoneNumber()
+    }
+  }
+  
+  var listings: [Vehicle]
 }
--- CarfaxIOS/Model/ViewModel/VehiclesManager.swift
+++ CarfaxIOS/Model/ViewModel/VehiclesManager.swift
@@ -14,68 +14,6 @@
 
 import UIKit
 
-struct VehicleImages: Codable {
-  var large: String
-  var medium: String
-  var small: String
-}
-
-struct Dealer: Codable {
-  var city: String
-  var state: String
-  var zip: String
-  var phone: String
-}
-
-struct Response: Codable {
-  
-  struct Vehicle: Codable {
-    var year: Int
-    var make: String
-    var model: String
-    var trim: String
-    var listPrice: Int
-    var mileage: Int
-    
-    var dealer: Dealer
-    
-    func formattedYearMakeModelTrim () -> String {
-      return String(year) + " " + make + model + trim
-    }
-    
-    func formattedPriceMileageLocation () -> NSAttributedString {
-      
-      let StringPrice = listPrice.currencyFormat
-      let StringMileage = mileage.abbreviated
-      
-      let mileageCityState = " | " + StringMileage + "Mi | " + dealer.city + ", " + dealer.state
-      
-      //now we want the price to be bold and rest of the string as regular font
-      let attributesBold: [NSAttributedString.Key: Any] = [
-        .font: Constants.systemFontBoldSize12,
-        .foregroundColor: Constants.blackColor
-      ]
-      
-      let attributesRegular: [NSAttributedString.Key: Any] = [
-        .font: Constants.systemFontSize12,
-        .foregroundColor: Constants.blackColor
-      ]
-      
-      let attributedString = NSMutableAttributedString(string: StringPrice, attributes: attributesBold)
-      let attributedStringForMileageCityState = NSAttributedString(string: mileageCityState, attributes: attributesRegular)
-      
-      attributedString.append(attributedStringForMileageCityState)
-      return attributedString
-    }
-    
-    func formattedPhoneNumber () -> String? {
-      return dealer.phone.formattedPhoneNumber()
-    }
-  }
-  
-  var listings: [Vehicle]
-}
-
 final class VehiclesManager: NSObject {
   
   // MARK: - Vars
@@ -94,12 +32,12 @@ final class VehiclesManager: NSObject {
       
         do {
           let decoder = JSONDecoder()
-          let gitData2 = try decoder.decode(Response.self, from: result )
-          self.vehiclesList.append(contentsOf: gitData2.listings)
+          let rootData = try decoder.decode(Response.self, from: result )
+          self.vehiclesList.append(contentsOf: rootData.listings)
         } catch let err {
           print("Err", err)
         }
-        //unpack all the data here
+      
         completion(true, "")
     }
   }
